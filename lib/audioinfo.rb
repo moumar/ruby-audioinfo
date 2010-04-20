2c5a36582e9a6ef56b51321cc8fb87fa958565d3
Marcello Barnaba <vjt@openssl.it>
2010-04-20 16:29:56 +0400

    [PATCH 9/9] Bumped to 0.1.6
    From a6c3e91add290a1b04697a6984f82760de45d857 Mon Sep 17 00:00:00 2001
    Date: Sat, 20 Mar 2010 17:26:03 +0100
    ---
     lib/audioinfo.rb |    2 +-
     1 files changed, 1 insertions(+), 1 deletions(-)

--- lib/audioinfo.rb
+++ lib/audioinfo.rb
@@ -28,7 +28,7 @@ class AudioInfo
 
   SUPPORTED_EXTENSIONS = %w{mp3 ogg mpc wma mp4 aac m4a flac}
 
-  VERSION = "0.1.5"
+  VERSION = "0.1.6"
 
   attr_reader :path, :extension, :musicbrainz_infos, :tracknum, :bitrate, :vbr
   attr_reader :artist, :album, :title, :length, :date
