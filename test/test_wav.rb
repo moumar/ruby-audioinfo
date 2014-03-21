ffa18a795f51940d42611d6ca02750a760dbf467
??? <billehs+github@gmail.com>
2014-03-21 10:08:36 -0400

    Add test

---
+++ test/test_wav.rb
@@ -0,0 +1,27 @@
+require_relative "../lib/audioinfo"
+require "minitest/autorun"
+
+require "fileutils"
+require "tmpdir"
+
+require_relative "test_helper"
+
+class TestWav < MiniTest::Unit::TestCase
+
+  WAV_FILE = "#{Dir.tmpdir}/ruby-audioinfo-test.wav" 
+
+  def setup
+    FileUtils.cp(File.join(SUPPORT_DIR, "piano2.wav"), WAV_FILE)
+  end
+
+  def test_wav_whitelist
+    i = AudioInfo.new(WAV_FILE)
+    assert_kind_of WaveInfo, i.info
+  end
+
+  def test_wav_length
+    i = AudioInfo.new(WAV_FILE)
+    assert_equal i.length, 6
+  end
+
+end
