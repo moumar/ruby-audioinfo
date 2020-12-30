10b8866d2e36e5e986e613ce46c7a77689c2a578
Jared Moody <jared@jaredmoody.com>
2020-12-29 22:33:29 -0800

    Codecov
    - Add config so codecov doesn't comment on commits
    - Disable codecov on Ruby 3
    - Add check for new codecov gem release

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter '/test/'
+end
+
+if ENV['CI']
+  require 'codecov'
+  if Gem::Version.new(Codecov::VERSION) > Gem::Version.new('0.2.15')
+    raise 'A new version of Codecov has been released, does it support Ruby3?'
+  end
+
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 
 require 'audioinfo'
