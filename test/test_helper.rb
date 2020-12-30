afb85244985a6e566e1d2e6e15ba9e44aaa86b42
Jared Moody <jared@jaredmoody.com>
2020-12-29 22:15:07 -0800

    Test Cleanup
    - Use Minittest::Test
    - require minitest/autorun in test_helper
    - require library in test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+
+require 'audioinfo'
+
+require 'minitest/autorun'
 require 'minitest/rg'
+
 SUPPORT_DIR = File.expand_path('support', File.dirname(__FILE__))
